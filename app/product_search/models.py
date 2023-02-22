5a04588f381850190ad095198674671b093efa8d
Andrey Erokhin <aeroalgo@gmail.com>
2023-02-22 09:56:54 +0300

    Отредактировал css, добавил картинки, наследование шаблонов

--- app/product_search/models.py
+++ app/product_search/models.py
@@ -162,16 +162,25 @@ class Last30DaysData(models.Model):
     )
 
 
+
 def image_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/product/id/<filename>
     return 'product/{0}/{1}'.format(instance.sku.id, filename)
 
 
 class ProductPhoto(models.Model):
-    sku = models.ForeignKey(Product, verbose_name="Фото sku", on_delete=models.CASCADE, related_name="photo")
-    link = models.TextField(verbose_name="Ссылка на фото", blank=True, null=True)
-    photo = models.ImageField(verbose_name="Фото", blank=True, null=True, upload_to=image_path)
-    filename = models.TextField(verbose_name="Имя файла", blank=True, null=True)
+    sku = models.ForeignKey(
+        Product, verbose_name="Фото sku", on_delete=models.CASCADE, related_name="photo"
+    )
+    link = models.TextField(
+        verbose_name="Ссылка на фото", blank=True, null=True
+    )
+    photo = models.ImageField(
+        verbose_name="Фото", blank=True, null=True, upload_to=image_path
+    )
+    filename = models.TextField(
+        verbose_name="Имя файла", blank=True, null=True
+    )
 
     def get_remote_image(self):
         if self.link and not self.photo:
